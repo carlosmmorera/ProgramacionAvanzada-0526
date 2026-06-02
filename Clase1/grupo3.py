0c96d455c9a8bfd5b0785f6b9e80686db91b5d2c
Luis <luistorresdelnuevo@gmail.com>
2026-06-02 20:09:48 +0200

    Realización Función show_inventory_report

--- Clase1/grupo3.py
+++ Clase1/grupo3.py
@@ -1,21 +1,28 @@
-from Clase1.grupo2 import Product
+#from Clase1.grupo2 import Product
+
 
 def show_inventory_report(inventory: dict) -> None:
-    pass
+    print("\nInventory Report:\n")
+    for product in inventory.values():
+        category_names = ", ".join([cat["name"] for cat in product["categories"]]) or "None"
+        tag_names = ", ".join([tag["name"] for tag in product["tags"]]) or "None"
+        print(f"Product: {product['name']} (SKU: {product['sku']}) - Price: ${product['price']:.2f}, Stock: {product['current_stock']}, Categories: [{category_names}], Tags: [{tag_names}]")
+
 
 
 class Inventory:
     def __init__(self):
-        ...
+        pass
 
     def add_product(self, product: Product) -> None:
-        ...
+        pass
 
     def get_product(self, sku: str) -> Product | None:
-        ...
+        pass
 
     def generate_inventory_report(self) -> str:
-        ...
+        pass
 
     def __repr__(self) -> str:
-        ...
+        pass
+
