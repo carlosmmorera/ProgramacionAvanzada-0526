23b95917129e605b63b10e2b7e79029671cd0a9f
SergioPeris.IA0526 <serpegon11710@gmail.com>
2026-06-02 20:16:15 +0200

    Update grupo2.py

--- Clase1/grupo2.py
+++ Clase1/grupo2.py
@@ -1,7 +1,19 @@
 from typing import Optional
+from grupo1 import check_stock
+
 
 def update_stock(product: dict, sold_units: int) -> dict:
-    return {}
+
+    if product is None:
+        print(f"Error: Product not found.")
+        return {}
+    
+    if check_stock(product["current_stock"], sold_units):
+        product["current_stock"]=product["current_stock"]-sold_units
+    else:
+        print(f"Error: Insufficient stock for {product['name']}. Available: {product['current_stock']}, Requested: {sold_units}")
+
+    return product
 
 
 
@@ -21,3 +33,11 @@ class Product:
 
     def __repr__(self) -> str:
         ...
+
+product = {"name": "Laptop", "sku": "SKU123", "price": 1200, "current_stock": 10, 
+           #"categories": [categories[0]], "tags": [tags[1], tags[2]]
+           }
+print (product)
+product=update_stock(product,2)
+
+print (product)
